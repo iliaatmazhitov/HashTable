8cd7cc15231988c1fc257e98d121df312065679c
iliaatmazhitov <atmazhitovilya@outlook.com>
2024-07-12 00:45:48 +0300

    Update UI

--- UI.cpp
+++ UI.cpp
@@ -2,9 +2,11 @@
 // Created by Ilya Atmazhitov.
 //
 
-#include "KeySpace.h"
 #include <iostream>
 #include <limits>
+#include <string>
+#include "KeySpace.h"
+#include "Person.h"
 
 const string BLUE = "\033[34m";
 const string GREEN = "\033[32m";
@@ -13,6 +15,7 @@ const string YELLOW = "\033[33m";
 const string RESET = "\033[0m";
 const string BOLD = "\033[1m";
 const string UNDERLINE = "\033[4m";
+
 
 
 void printMenu() {
@@ -29,5 +32,124 @@ void printMenu() {
 
 
 void runMenu() {
-    printMenu();
+    HashTable<Person> table(10);
+    int menu_choice;
+    while (true) {
+        printMenu();
+        if (!(cin >> menu_choice)) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << RED << "Invalid input. Please enter a number." << RESET << endl;
+            continue;
+        }
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        switch (menu_choice) {
+            case 1: {  // Insert item
+                string name, series, number, key;
+                int age;
+
+                cout << "Enter name: ";
+                getline(cin, name);
+
+                cout << "Enter age: ";
+                while (!(cin >> age)) {
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    cout << RED << "Invalid age. Please enter a number: " << RESET;
+                }
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+                cout << "Enter passport series: ";
+                getline(cin, series);
+
+                cout << "Enter passport number: ";
+                getline(cin, number);
+
+                cout << "Enter key: ";
+                getline(cin, key);
+
+                Person person(name, age, series.c_str(), number.c_str());
+
+                try {
+                    table.Insert(key.c_str(), person);
+                    cout << GREEN << "Item inserted successfully." << RESET << endl;
+                } catch (const invalid_argument& e) {
+                    cout << RED << "Error: " << e.what() << RESET << endl;
+                }
+                break;
+            }
+            case 2: {  // Get item
+                string key;
+                int release;
+
+                cout << "Enter key: ";
+                getline(cin, key);
+
+                cout << "Enter release: ";
+                while (!(cin >> release)) {
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    cout << RED << "Invalid release. Please enter a number: " << RESET;
+                }
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+                try {
+                    Person person = table.Get(key.c_str(), release);
+                    cout << GREEN << "Item found: " << person << RESET << endl;
+                } catch (const exception& e) {
+                    cout << RED << "Error: Item not found." << RESET << endl;
+                }
+                break;
+            }
+            case 3: {  // Search key
+                string key;
+                cout << "Enter key to search: ";
+                getline(cin, key);
+                table.Search(key.c_str());
+                break;
+            }
+            case 4: {  // Delete release
+                string key;
+                int release;
+
+                cout << "Enter key: ";
+                getline(cin, key);
+
+                cout << "Enter release to delete: ";
+                while (!(cin >> release)) {
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    cout << RED << "Invalid release. Please enter a number: " << RESET;
+                }
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+                table.DelRelease(key.c_str(), release);
+                cout << GREEN << "Release deleted (if it existed)." << RESET << endl;
+                break;
+            }
+            case 5: {  // Delete key
+                string key;
+                cout << "Enter key to delete: ";
+                getline(cin, key);
+
+                table.DelKey(key.c_str());
+                cout << GREEN << "Key deleted (if it existed)." << RESET << endl;
+                break;
+            }
+            case 6: {  // Print table
+                table.PrintTable();
+                break;
+            }
+            case 0: {  // Exit
+                cout << "Exiting program." << endl;
+                return;
+            }
+            default: {
+                cout << RED << "Invalid choice. Please try again." << RESET << endl;
+                break;
+            }
+        }
+        cout << endl;
+    }
 }
--- main.cpp
+++ main.cpp
@@ -3,139 +3,139 @@
 #include "KeySpace.h"
 
 int main() {
-    HashTable<Person> h1(3);
-    Person p1("Ivan", 19);
-    Person p2("Nikolay", 19, "4529", "866678");
-    Person p3("Ilya", 19, "2929", "998774");
-    Person p4("Petr", 18, "1535", "47748");
-    Person p5("Maxim", 19, "6789", "123456");
-    Person p6("Denis", 17, "1525", "354555");
-    Person p7("Anna", 25, "7890", "234567");
-    Person p8("Olga", 30, "3456", "789012");
-    Person p9("Sergey", 22, "9012", "345678");
-    Person p10("Dmitry", 28, "5678", "901234");
+//    HashTable<Person> h1(3);
+//    Person p1("Ivan", 19);
+//    Person p2("Nikolay", 19, "4529", "866678");
+//    Person p3("Ilya", 19, "2929", "998774");
+//    Person p4("Petr", 18, "1535", "47748");
+//    Person p5("Maxim", 19, "6789", "123456");
+//    Person p6("Denis", 17, "1525", "354555");
+//    Person p7("Anna", 25, "7890", "234567");
+//    Person p8("Olga", 30, "3456", "789012");
+//    Person p9("Sergey", 22, "9012", "345678");
+//    Person p10("Dmitry", 28, "5678", "901234");
+//
+//    h1.Insert("HR", p7);
+//    h1.Insert("Managers", p8);
+//    h1.Insert("Developers", p9);
+//    h1.Insert("Designers", p10);
+//
+//    Person p11("Elena", 27, "1234", "567890");
+//    Person p12("Andrey", 33, "7890", "123456");
+//    Person p13("Tatiana", 29, "4567", "890123");
+//    Person p14("Alexey", 31, "0123", "456789");
+//    Person p15("Maria", 26, "6789", "012345");
+//
+//    h1.Insert("Accountants", p11);
+//    h1.Insert("Sales", p12);
+//    h1.Insert("Marketing", p13);
+//    h1.Insert("Support", p14);
+//    h1.Insert("QA", p15);
+//
+//
+//// Продолжаем добавлять еще 85 записей...
+//
+//    Person p16("Vladimir", 35, "2345", "678901");
+//    Person p17("Ekaterina", 24, "8901", "234567");
+//    Person p18("Nikolai", 29, "5678", "901234");
+//    Person p19("Irina", 27, "1234", "567890");
+//    Person p20("Mikhail", 32, "7890", "123456");
+//
+//    h1.Insert("Analysts", p16);
+//    h1.Insert("Researchers", p17);
+//    h1.Insert("Testers", p18);
+//    h1.Insert("Designers", p19);
+//    h1.Insert("Managers", p20);
+//
+//// ... продолжайте в том же духе, создавая новые объекты Person
+//// и вставляя их в хеш-таблицу с различными ключами
+//
+//    Person p96("Boris", 45, "6789", "012345");
+//    Person p97("Galina", 38, "2345", "678901");
+//    Person p98("Yuri", 29, "8901", "234567");
+//    Person p99("Valentina", 33, "4567", "890123");
+//    Person p100("Grigory", 27, "0123", "456789");
+//
+//    h1.Insert("Directors", p96);
+//    h1.Insert("Consultants", p97);
+//    h1.Insert("Trainers", p98);
+//    h1.Insert("Coordinators", p99);
+//    h1.Insert("Specialists", p100);
+//
+//    Person p101("Svetlana", 28, "5678", "901234");
+//    Person p102("Igor", 36, "1234", "567890");
+//    Person p103("Natalya", 31, "7890", "123456");
+//    Person p104("Oleg", 29, "3456", "789012");
+//    Person p105("Ksenia", 25, "9012", "345678");
+//
+//    h1.Insert("Lawyers", p101);
+//    h1.Insert("Engineers", p102);
+//    h1.Insert("Accountants", p103);
+//    h1.Insert("Developers", p104);
+//    h1.Insert("Designers", p105);
+//
+//    Person p106("Artem", 33, "5678", "901234");
+//    Person p107("Polina", 27, "1234", "567890");
+//    Person p108("Roman", 30, "7890", "123456");
+//    Person p109("Alina", 26, "3456", "789012");
+//    Person p110("Vadim", 34, "9012", "345678");
+//
+//    h1.Insert("Managers", p106);
+//    h1.Insert("HR", p107);
+//    h1.Insert("Sales", p108);
+//    h1.Insert("Marketing", p109);
+//    h1.Insert("Support", p110);
+//
+//    Person p111("Yulia", 29, "5678", "901234");
+//    Person p112("Stanislav", 35, "1234", "567890");
+//    Person p113("Evgenia", 28, "7890", "123456");
+//    Person p114("Kirill", 32, "3456", "789012");
+//    Person p115("Daria", 27, "9012", "345678");
+//
+//    h1.Insert("Analysts", p111);
+//    h1.Insert("Researchers", p112);
+//    h1.Insert("Testers", p113);
+//    h1.Insert("Programmers", p114);
+//    h1.Insert("Designers", p115);
+//
+//    Person p116("Timur", 31, "5678", "901234");
+//    Person p117("Veronika", 26, "1234", "567890");
+//    Person p118("Gleb", 33, "7890", "123456");
+//    Person p119("Karina", 29, "3456", "789012");
+//    Person p120("Leonid", 35, "9012", "345678");
+//
+//    h1.Insert("Economists", p116);
+//    h1.Insert("Coders", p117);
+//    h1.Insert("Managers", p118);
+//    h1.Insert("HR", p119);
+//    h1.Insert("Sales", p120);
+//
+//    Person p121("Yana", 28, "5678", "901234");
+//    Person p122("Ruslan", 34, "1234", "567890");
+//    Person p123("Zlata", 27, "7890", "123456");
+//    Person p124("Mikhail", 30, "3456", "789012");
+//    Person p125("Nina", 32, "9012", "345678");
+//
+//    h1.Insert("Marketing", p121);
+//    h1.Insert("Support", p122);
+//    h1.Insert("Developers", p123);
+//    h1.Insert("Designers", p124);
+//    h1.Insert("Analysts", p125);
+//
+//
+//
+//    h1.Insert("Economists", p1);
+//    h1.Insert("Coders", p2);
+//    h1.Insert("CEO", p3);
+//    h1.Insert("Coders", p4);
+//    h1.Insert("Economists", p5);
+//    h1.Insert("Bodyguard", p6);
+//
+//    h1.DelRelease("Coders", 3);
+//    h1.Search("Coders");
 
-    h1.Insert("HR", p7);
-    h1.Insert("Managers", p8);
-    h1.Insert("Developers", p9);
-    h1.Insert("Designers", p10);
 
-    Person p11("Elena", 27, "1234", "567890");
-    Person p12("Andrey", 33, "7890", "123456");
-    Person p13("Tatiana", 29, "4567", "890123");
-    Person p14("Alexey", 31, "0123", "456789");
-    Person p15("Maria", 26, "6789", "012345");
-
-    h1.Insert("Accountants", p11);
-    h1.Insert("Sales", p12);
-    h1.Insert("Marketing", p13);
-    h1.Insert("Support", p14);
-    h1.Insert("QA", p15);
-
-
-// Продолжаем добавлять еще 85 записей...
-
-    Person p16("Vladimir", 35, "2345", "678901");
-    Person p17("Ekaterina", 24, "8901", "234567");
-    Person p18("Nikolai", 29, "5678", "901234");
-    Person p19("Irina", 27, "1234", "567890");
-    Person p20("Mikhail", 32, "7890", "123456");
-
-    h1.Insert("Analysts", p16);
-    h1.Insert("Researchers", p17);
-    h1.Insert("Testers", p18);
-    h1.Insert("Designers", p19);
-    h1.Insert("Managers", p20);
-
-// ... продолжайте в том же духе, создавая новые объекты Person
-// и вставляя их в хеш-таблицу с различными ключами
-
-    Person p96("Boris", 45, "6789", "012345");
-    Person p97("Galina", 38, "2345", "678901");
-    Person p98("Yuri", 29, "8901", "234567");
-    Person p99("Valentina", 33, "4567", "890123");
-    Person p100("Grigory", 27, "0123", "456789");
-
-    h1.Insert("Directors", p96);
-    h1.Insert("Consultants", p97);
-    h1.Insert("Trainers", p98);
-    h1.Insert("Coordinators", p99);
-    h1.Insert("Specialists", p100);
-
-    Person p101("Svetlana", 28, "5678", "901234");
-    Person p102("Igor", 36, "1234", "567890");
-    Person p103("Natalya", 31, "7890", "123456");
-    Person p104("Oleg", 29, "3456", "789012");
-    Person p105("Ksenia", 25, "9012", "345678");
-
-    h1.Insert("Lawyers", p101);
-    h1.Insert("Engineers", p102);
-    h1.Insert("Accountants", p103);
-    h1.Insert("Developers", p104);
-    h1.Insert("Designers", p105);
-
-    Person p106("Artem", 33, "5678", "901234");
-    Person p107("Polina", 27, "1234", "567890");
-    Person p108("Roman", 30, "7890", "123456");
-    Person p109("Alina", 26, "3456", "789012");
-    Person p110("Vadim", 34, "9012", "345678");
-
-    h1.Insert("Managers", p106);
-    h1.Insert("HR", p107);
-    h1.Insert("Sales", p108);
-    h1.Insert("Marketing", p109);
-    h1.Insert("Support", p110);
-
-    Person p111("Yulia", 29, "5678", "901234");
-    Person p112("Stanislav", 35, "1234", "567890");
-    Person p113("Evgenia", 28, "7890", "123456");
-    Person p114("Kirill", 32, "3456", "789012");
-    Person p115("Daria", 27, "9012", "345678");
-
-    h1.Insert("Analysts", p111);
-    h1.Insert("Researchers", p112);
-    h1.Insert("Testers", p113);
-    h1.Insert("Programmers", p114);
-    h1.Insert("Designers", p115);
-
-    Person p116("Timur", 31, "5678", "901234");
-    Person p117("Veronika", 26, "1234", "567890");
-    Person p118("Gleb", 33, "7890", "123456");
-    Person p119("Karina", 29, "3456", "789012");
-    Person p120("Leonid", 35, "9012", "345678");
-
-    h1.Insert("Economists", p116);
-    h1.Insert("Coders", p117);
-    h1.Insert("Managers", p118);
-    h1.Insert("HR", p119);
-    h1.Insert("Sales", p120);
-
-    Person p121("Yana", 28, "5678", "901234");
-    Person p122("Ruslan", 34, "1234", "567890");
-    Person p123("Zlata", 27, "7890", "123456");
-    Person p124("Mikhail", 30, "3456", "789012");
-    Person p125("Nina", 32, "9012", "345678");
-
-    h1.Insert("Marketing", p121);
-    h1.Insert("Support", p122);
-    h1.Insert("Developers", p123);
-    h1.Insert("Designers", p124);
-    h1.Insert("Analysts", p125);
-
-
-
-    h1.Insert("Economists", p1);
-    h1.Insert("Coders", p2);
-    h1.Insert("CEO", p3);
-    h1.Insert("Coders", p4);
-    h1.Insert("Economists", p5);
-    h1.Insert("Bodyguard", p6);
-
-    h1.DelRelease("Coders", 3);
-    h1.Search("Coders");
-
-
-    h1.PrintTable();
+    runMenu();
 
 
 //
